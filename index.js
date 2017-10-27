4b3324a67d86a97a213c4587b6c9d88faad07161
Ovidiu Spatacian-Tarnu <github@zoosh.me>
2017-10-27 13:55:28 +0100

    adds proper content-type header for static assets

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const path = require('path');
 const url = 'mongodb://zooshme:ovi1982aug@ds127375.mlab.com:27375/cakes';
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.doc': 'application/msword',
+  '.eot': 'appliaction/vnd.ms-fontobject',
+  '.ttf': 'aplication/font-sfnt'
+}
 
 function connect(callback) {
   MongoClient.connect(url, callback);
 }
+
+
 
 module.exports = async (req, res) => {
   console.log(req.url)
   if (req.method === 'GET' && req.url.match(/\.(jpeg|jpg|gif|ico|png|css|js|svg)$/)) {
     let filePath = path.join(__dirname, 'assets', req.url);
     if (fs.existsSync(filePath)) {
+      const ext = path.parse(filePath).ext;
+      res.setHeader('Content-Type', mimeType[ext] || 'text/plain');
       res.end(fs.readFileSync(filePath));
     }
   } else if (req.method === 'GET' && (req.url.match(/^\/$/) || req.url.match(/^\/(?!api\/)[a-zA-Z0-9\/]+$/))) {
